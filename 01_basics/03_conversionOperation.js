822955684ec2c9e99c8c440e7b665b41064a17d7
Ankit <ankitkulkarni2745@gmail.com>
2026-06-26 21:34:30 +0530

    Learned basic conversions and arithmatic operations

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,35 @@
+/*
+let score = undefined
+console.log(typeof score)
+console.log(typeof (score))
+let valueInNumber = Number(score) // Method to convert the string to number
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+//"33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+let isLoggedIn = ""
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(typeof booleanIsLoggedIn)
+console.log(booleanIsLoggedIn)
+// 1 => true; 0 => false
+// "" => false
+//"Hitesh" => true
+let number = 33
+let stringNumber = String(number)
+console.log(typeof stringNumber)
+console.log(stringNumber)
+*/
+// ***************** Operations *****************
+let value = 3
+let negValue = -value
+console.log(negValue)
+//Read all the arithmatic operations once
+let str1 = "Hello "
+let str2 = "Ankit"
+let str3 = str1 + str2
+console.log(str3)
+console.log("1"+ 2)
+console.log( 1 + "2" + 2) //Note o/p = 122
+console.log(1+2+"2") // Note o/p => 32
+//Read prefix and postfix increment/decrement operator from mdn docs
