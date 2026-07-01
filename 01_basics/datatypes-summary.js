b77099093b5d87f77444bad5913968ebc7aad7f8
Ankit <ankitkulkarni2745@gmail.com>
2026-07-01 16:24:46 +0530

    Datatypes su,,ary for interviews

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,62 @@
+/*
+----------------------------Data-types summary for Interviews-----------------------------------------
+JavaScript is a dynamically typed language.
+This means that you do not need to explicitly declare a variable's data type when creating it,
+and the interpreter assigns a type at runtime based on its current value.
+Furthermore, a single variable can change its data type throughout the execution of your program
+*/
+
+//  Primitive
+
+//  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+// const bigNumber = 3456543576654356754n
+
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+    name: "hitesh",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof anotherId);
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
+
+/*Important:
+Return type of variables in JavaScript
+1) Primitive Datatypes
+       Number => number
+       String  => string
+       Boolean  => boolean
+       null  => object
+       undefined  =>  undefined
+       Symbol  =>  symbol
+       BigInt  =>  bigint
+
+2) Non-primitive Datatypes
+       Arrays  =>  object
+       Function  =>  function
+       Object  =>  object
+*/
