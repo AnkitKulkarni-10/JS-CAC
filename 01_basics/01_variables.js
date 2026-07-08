59e8571daaad593465a637cb4cc6e02b43809cc2
Ankit <ankitkulkarni2745@gmail.com>
2026-07-08 16:23:49 +0530

    Js variables

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -13,3 +13,12 @@ Prefer not to use var
 Because of issue in block-scope and functional scope
 */
 console.table([accountId, accountPassword , accountEmail, accountCity, accountState]);
+
+/* Q: Why avoid var: 
+-var is function scoped-> Ignores boundaries like if statements or for loops.
+eg. If you declare variable var i inside a for loop for most laguages it is only,
+accessible in the loop. Whereas in JS i can be accesses outside of the loop as well
+which throws errors.
+-You can "Re-declare" same var variable multipe times in same scope-> Js does not throw
+an error which can cause you to overrite data accidentally
+*/
