f8950f66f4f539bc4396057db7971c7256f7f660
Ankit <ankitkulkarni2745@gmail.com>
2026-07-08 17:28:11 +0530

    Learned basic conversions and arithmatic operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,10 +1,10 @@
 /*
 let score = undefined
-console.log(typeof score)
+console.log(typeof score) //undefined
 console.log(typeof (score))
 let valueInNumber = Number(score) // Method to convert the string to number
 console.log(typeof valueInNumber)
-console.log(valueInNumber)
+console.log(valueInNumber) //NaN
 //"33" => 33
 // "33abc" => NaN
 // true => 1; false => 0
@@ -24,6 +24,8 @@ console.log(stringNumber)
 let value = 3
 let negValue = -value
 console.log(negValue)
+console.log(value);
+
 //Read all the arithmatic operations once
 let str1 = "Hello "
 let str2 = "Ankit"
@@ -32,4 +34,8 @@ console.log(str3)
 console.log("1"+ 2)
 console.log( 1 + "2" + 2) //Note o/p = 122
 console.log(1+2+"2") // Note o/p => 32
+console.log(1 + "2" + 2 +"2" + 1);// 12221
+/*Note: Easy way to remember:
+"left-to-right evaluation", and once a string appears, everything after becomes concatenation.
+*/
 //Read prefix and postfix increment/decrement operator from mdn docs
