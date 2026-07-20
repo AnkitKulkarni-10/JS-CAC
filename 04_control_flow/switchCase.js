639763fcc141c5af51d0fc3c9fc833b7ede4bb66
Ankit <ankitkulkarni2745@gmail.com>
2026-07-20 23:45:15 +0530

    Learned Swich Case in JS

---
+++ 04_control_flow/switchCase.js
@@ -0,0 +1,45 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+// Note: If the break statement is not used here, after the match, the entire code gets executed "Except the default condition"
+/*
+const month = 3
+switch (month){
+    case 1 : 
+    console.log("January");
+    break;
+    case 2 : 
+    console.log("February");
+    break;
+    case 3 : 
+    console.log("March");
+    break;
+    case 4 : 
+    console.log("April");
+    break;
+    default : 
+    console.log("Default case matched");
+}
+*/ //OR
+const month = "April"
+switch (month){
+    case "January" : 
+    console.log("January");
+    break;
+    case "February" : 
+    console.log("February");
+    break;
+    case "March" : 
+    console.log("March");
+    break;
+    case "April" : 
+    console.log("April");
+    break;
+    default : 
+    console.log("Default case matched");
+}
