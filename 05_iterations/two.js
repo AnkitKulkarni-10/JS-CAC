706b180aefd23c04e8b34329e79d5cb337d1b6dc
Ankit <ankitkulkarni2745@gmail.com>
2026-07-21 23:36:30 +0530

    Learned while and do-while in JS

---
+++ 05_iterations/two.js
@@ -0,0 +1,24 @@
+// while loop
+let index = 0
+while (index<=10) {
+    console.log(`Value of index is : ${index}`);
+    index+= 2
+    
+}
+
+let myArray = ['flash', 'batman', 'superman']
+let i = 0;
+while(i < myArray.length)
+{
+    console.log(`Value is : ${myArray[i]}`);
+    i++
+    
+}
+// do-while loop
+let score = 1
+
+do {
+    console.log(`Score is : ${score}`);
+    score++
+    
+} while (score <= 10);
