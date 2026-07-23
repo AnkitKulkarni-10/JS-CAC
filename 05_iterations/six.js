e36dae71227e39197964acddc8d92dabfc153689
Ankit <ankitkulkarni2745@gmail.com>
2026-07-23 22:47:06 +0530

    Learned filter() method

---
+++ 05_iterations/six.js
@@ -0,0 +1,49 @@
+const coding  = ["js", "rb", "python", "java", "cpp"]
+const values = coding.forEach( (item)=> {
+    //console.log(item);
+    return item
+} )
+//console.log(values); //undefined
+
+/*forEach loop does not return any value by itself.
+Use forEach() when you want to do something with every item (like saving to a database, updating the DOM, or logging),
+but you do not need to create a new array. */
+
+//filter() method: filter() creates a new array containing only elements that pass a "specific condition"
+
+const myNums = [1,2,3,4,5,6,7,8,9,10]
+// const newNums = myNums.filter( (num) => num > 4 ) ->Implicit return
+const newNums = myNums.filter( (num) => {
+    return num > 4; //Explicit return
+} )
+//console.log(newNums);
+
+//Doing this using forEach :
+const Nums = []
+myNums.forEach((num)=> {
+    if(num>4){
+        Nums.push(num)
+    }
+} )
+//console.log(Nums);
+
+//Practicing filter() method:
+const books = [
+    { title: 'Book One', genre: 'Fiction', publish: 1981, edition: 2004 },
+    { title: 'Book Two', genre: 'Non-Fiction', publish: 1992, edition: 2008 },
+    { title: 'Book Three', genre: 'History', publish: 1999, edition: 2007 },
+    { title: 'Book Four', genre: 'Non-Fiction', publish: 1989, edition: 2010 },
+    { title: 'Book Five', genre: 'Science', publish: 2009, edition: 2014 },
+    { title: 'Book Six', genre: 'Fiction', publish: 1987, edition: 2010 },
+    { title: 'Book Seven', genre: 'History', publish: 1986, edition: 1996 },
+    { title: 'Book Eight', genre: 'Science', publish: 2011, edition: 2016 },
+    { title: 'Book Nine', genre: 'Non-Fiction', publish: 1981, edition: 1989 },
+  ];
+
+  let userBooks = books.filter( (bk) => bk.genre === 'History')
+
+  userBooks = books.filter( (bk) => { 
+    return bk.publish >= 1995 && bk.genre === "History"
+})
+  console.log(userBooks);
+
