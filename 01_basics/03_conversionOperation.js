f880ffbae1e7e498a7e242c1c8385744ef6d008c
Ankit <ankitkulkarni2745@gmail.com>
2026-08-22 23:53:37 +0530

    Basic conversions and arithmatic operations (Revised)

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,23 +1,23 @@
 /*
 let score = undefined
 console.log(typeof score) //undefined
-console.log(typeof (score))
+console.log(typeof (score))//same as above 
 let valueInNumber = Number(score) // Method to convert the string to number
 console.log(typeof valueInNumber)
 console.log(valueInNumber) //NaN
 //"33" => 33
-// "33abc" => NaN
+// "33abc" When converted to number gives => NaN
 // true => 1; false => 0
 let isLoggedIn = ""
-let booleanIsLoggedIn = Boolean(isLoggedIn)
+let booleanIsLoggedIn = Boolean(isLoggedIn) //Conversion To Boolean
 console.log(typeof booleanIsLoggedIn)
 console.log(booleanIsLoggedIn)
 // 1 => true; 0 => false
 // "" => false
-//"Hitesh" => true
+//"Hitesh" => true (Diff case*)
 let number = 33
 let stringNumber = String(number)
-console.log(typeof stringNumber)
+console.log(typeof stringNumber) //String
 console.log(stringNumber)
 */
 // ***************** Operations *****************
@@ -31,7 +31,7 @@ let str1 = "Hello "
 let str2 = "Ankit"
 let str3 = str1 + str2
 console.log(str3)
-console.log("1"+ 2)
+console.log("1"+ 2) //12
 console.log( 1 + "2" + 2) //Note o/p = 122
 console.log(1+2+"2") // Note o/p => 32
 console.log(1 + "2" + 2 +"2" + 1);// 12221
